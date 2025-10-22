4e2b102b87a169c8beffe62b50e09987c0148aef
dasb12345 <106763828+dasb12345@users.noreply.github.com>
2025-10-22 15:42:25 +0200

    fix path 2

--- frontend/src/merch.ts
+++ frontend/src/merch.ts
@@ -1,31 +1,31 @@
 export const merchProducts = [
     {
         id: '1',
-        imageUrls: ['merch/basic-tee-front.png', 'merch/basic-tee-left.png'],
+        imageUrls: ['/merch/basic-tee-front.png', '/merch/basic-tee-left.png'],
         title: 'Basic Tee',
         price: '',
     },
     {
         id: '2',
-        imageUrls: ['merch/tour-hoodie-back.png', 'merch/tour-hoodie-front.png', 'merch/tour-hoodie-left.png'],
+        imageUrls: ['/merch/tour-hoodie-back.png', '/merch/tour-hoodie-front.png', '/merch/tour-hoodie-left.png'],
         title: 'Tour Hoodie',
         price: '',
     },
     {
         id: '3',
-        imageUrls: ['merch/tour-basic-tee-back.png', 'merch/tour-basic-tee-front.png', 'merch/tour-basic-tee-left.png'],
+        imageUrls: ['/merch/tour-basic-tee-back.png', '/merch/tour-basic-tee-front.png', '/merch/tour-basic-tee-left.png'],
         title: 'Basic Tour Tee',
         price: '',
     },
     {
         id: '4',
-        imageUrls: ['merch/premium-tee-front.png', 'merch/premium-tee-left.png'],
+        imageUrls: ['/merch/premium-tee-front.png', '/merch/premium-tee-left.png'],
         title: 'Premium Tee',
         price: '',
     },
     {
         id: '5',
-        imageUrls: ['merch/bear-front.png', 'merch/bear-back.png'],
+        imageUrls: ['/merch/bear-front.png', '/merch/bear-back.png'],
         title: 'Bamse',
         price: '',
     },
