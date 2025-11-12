ede3f119b62c5014c9f89bbe6e2ba10f9ba50433
roryff <rory.fitzg@hotmail.com>
2025-11-12 23:08:01 +0100

    merchity

--- backend/src/data/products.ts
+++ backend/src/data/products.ts
@@ -49,6 +49,22 @@ export const products: Product[] = [
         description: 'Adorable teddy bear mascot',
     },
     {
+        id: '6',
+        imageUrls: ['/merch/boxer/boxer-back.png', '/merch/boxer/boxer-front.png'],
+        title: 'Boxer Shorts',
+        price: '299',
+        sizes: ['S', 'M', 'L', 'XL', 'XXL'],
+        description: '- Med omsluttende, fremhevet gummibånd\n - Elastisk jersey, behagelig tøybar og myk å ta på\n - Myk stoffkvalitet: 160 g/m²\n - Materiale: 95 % bomull, 5 % elastan\n - Sertifikater: Global Recycled Standard, GOTS, OEKO-TEX® STANDARD 100\n - Lettstelt: 30 °C vaskbar, kan strykes',
+    },
+    {
+        id: '7',
+        imageUrls: ['/merch/tank-top/tank-front.png', '/merch/tank-top/tank-back.png'],
+        title: 'Tank Top',
+        price: '299',
+        sizes: ['XS', 'S', 'M', 'L', 'XL', 'XXL'],
+        description: '- Normalt snitt for optimal passform\n - 1x1 ribbestrikk i halsutsnitt og ermehull\n - Dobbelt søm i livet og på ermene\n - Fargetilpasset jersey-nakkebånd\n - Satengetikett innsydd på siden\n - Nøytral størrelsesetikett\n - Gjennomsnittlig Stoffkvalitet: 200 g/m²\n - Materiale: 100 % kjemmet økologisk bomull',
+    },
+    {
         id: '69',
         imageUrls: ['/merch/west/west_front.png', '/merch/west/west_back.png'],
         title: 'Bamse',
