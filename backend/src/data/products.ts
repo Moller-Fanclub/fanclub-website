99791cabf36faa0e650374d0e996aea80ff5afeb
roryff <rory.fitzg@hotmail.com>
2025-11-12 23:23:36 +0100

    remove preview merch

--- backend/src/data/products.ts
+++ backend/src/data/products.ts
@@ -9,46 +9,6 @@ export interface Product {
 
 export const products: Product[] = [
     {
-        id: '1',
-        imageUrls: ['/merch/basic-tee/basic-tee-front.png', '/merch/basic-tee/basic-tee-left.png'],
-        title: 'Basic Tee',
-        price: '349',
-        sizes: ['XS', 'S', 'M', 'L', 'XL', 'XXL'],
-        description: 'Classic basic tee in premium cotton',
-    },
-    {
-        id: '2',
-        imageUrls: ['/merch/tour-hoodie/tour-hoodie-back.png', '/merch/tour-hoodie/tour-hoodie-front.png', '/merch/tour-hoodie/tour-hoodie-left.png'],
-        title: 'Tour Hoodie',
-        price: '649',
-        sizes: ['S', 'M', 'L', 'XL', 'XXL'],
-        description: 'Comfortable hoodie perfect for cold race days',
-    },
-    {
-        id: '3',
-        imageUrls: ['/merch/tour-basic/tour-basic-tee-back.png', '/merch/tour-basic/tour-basic-tee-front.png', '/merch/tour-basic/tour-basic-tee-left.png'],
-        title: 'Basic Tour Tee',
-        price: '399',
-        sizes: ['XS', 'S', 'M', 'L', 'XL', 'XXL'],
-        description: 'Tour edition basic tee with special design',
-    },
-    {
-        id: '4',
-        imageUrls: ['/merch/premium-tee/premium-tee-front.png', '/merch/premium-tee/premium-tee-left.png'],
-        title: 'Premium Tee',
-        price: '449',
-        sizes: ['S', 'M', 'L', 'XL'],
-        description: 'Premium quality t-shirt for true fans',
-    },
-    {
-        id: '5',
-        imageUrls: ['/merch/bear/bear-front.png', '/merch/bear/bear-back.png'],
-        title: 'Bamse',
-        price: '299',
-        sizes: ['One Size'],
-        description: 'Adorable teddy bear mascot',
-    },
-    {
         id: '6',
         imageUrls: ['/merch/boxer/boxer-back.png', '/merch/boxer/boxer-front.png'],
         title: 'Boxer Shorts',
